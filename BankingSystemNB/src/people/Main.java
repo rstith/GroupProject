37d1e8c4b4d0b5e5dce129e886d0ada1c614d7be
Jordan Fjellman <Jordan.Fjellman@Gmail.com>
2014-11-15 20:36:34 -0600

    Starting to add the People class along with the different types of people.

---
+++ BankingSystemNB/src/people/Main.java
@@ -0,0 +1,13 @@
+package people;
+
+public class Main {
+
+	public static void main(String[] args){
+	
+		Person testPerson = new Person("P00001", "Jordan", "Fjellman", "239890983", "243 North Road St.", "Saint Joseph", "MO", "23451");
+
+		testPerson.print();
+	
+	}//End method main
+
+}//End class main
